92463ad3e31f6ba39eacbe5061a7d6c7f3dffca0
Abedela Bedru <a2dul.12a@gmail.com>
2024-07-29 02:39:06 +0300

    fixed some security issues

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
 const express = require("express");
 const path = require("path");
 const methodOverride = require("method-override");
@@ -6,11 +10,13 @@ const mongoose = require("mongoose");
 const session = require("express-session");
 const flash = require("connect-flash");
 const ExpressError = require("./utils/ExpressError");
-const passport = require('passport')
-const LocalStrategy = require('passport-local')
-const User = require('./models/user')
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user");
+const mongoSanitize = require("express-mongo-sanitize");
+const helmet = require("helmet");
 
-const userRoutes = require('./Routes/users')
+const userRoutes = require("./Routes/users");
 const campgroundRoutes = require("./Routes/campground");
 const reviewsRoutes = require("./Routes/reviews");
 
@@ -31,40 +37,88 @@ app.engine("ejs", ejsMate);
 app.use(methodOverride("_method"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(mongoSanitize());
 
 const sessionConfig = {
+  name: "safer",
   secret: "thisshouldbeasecret",
   resave: false,
   saveUninitialized: false,
   cookie: {
     httpOnly: true,
+    // secure: true,
     expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
-    maxAge: 600000
+    maxAge: 600000,
   },
 };
 app.use(session(sessionConfig));
 app.use(flash());
+app.use(helmet({ contentSecurityPolicy: false }));
 
-app.use(passport.initialize())
-app.use(passport.session())
+const scriptSrcUrls = [
+  "https://stackpath.bootstrapcdn.com/",
+  "https://api.tiles.mapbox.com/",
+  "https://api.mapbox.com/",
+  "https://kit.fontawesome.com/",
+  "https://cdnjs.cloudflare.com/",
+  "https://cdn.jsdelivr.net",
+];
+const styleSrcUrls = [
+  "https://kit-free.fontawesome.com/",
+  "https://stackpath.bootstrapcdn.com/",
+  "https://api.mapbox.com/",
+  "https://api.tiles.mapbox.com/",
+  "https://fonts.googleapis.com/",
+  "https://use.fontawesome.com/",
+  "https://cdn.jsdelivr.net",
+];
+const connectSrcUrls = [
+  "https://api.mapbox.com/",
+  "https://a.tiles.mapbox.com/",
+  "https://b.tiles.mapbox.com/",
+  "https://events.mapbox.com/",
+];
+const fontSrcUrls = [];
+app.use(
+  helmet.contentSecurityPolicy({
+    useDefaults: false,
+    directives: {
+      defaultSrc: [],
+      connectSrc: ["'self'", ...connectSrcUrls],
+      scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+      styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+      workerSrc: ["'self'", "blob:"],
+      objectSrc: [],
+      imgSrc: [
+        "'self'",
+        "blob:",
+        "data:",
+        "https://res.cloudinary.com/ddzisjsqe/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT!
+        "https://api.unsplash.com/",
+      ],
+      fontSrc: ["'self'", ...fontSrcUrls],
+    },
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
 
-passport.serializeUser(User.serializeUser())
-passport.deserializeUser(User.deserializeUser())
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
-  console.log(req.session)
-  res.locals.currentUser = req.user
+  res.locals.currentUser = req.user;
   res.locals.success = req.flash("success");
-  res.locals.error = req.flash("error")
-  next()
-})
+  res.locals.error = req.flash("error");
+  next();
+});
 
-app.use('/', userRoutes);
+app.use("/", userRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/reviews", reviewsRoutes);
 app.use(express.static(path.join(__dirname + "/public")));
-
 
 app.get("/", (req, res) => {
   res.render("home");
@@ -81,5 +135,5 @@ app.use((err, req, res, next) => {
 });
 
 app.listen(3000, () => {
-  console.log("Connected on port 3000!");
+  console.log("Connected on port http://localhost:3000 !");
 });
